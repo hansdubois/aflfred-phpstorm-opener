b3b6074c9401c904593fa6e9999bfe81e46a5f99
Hans Dubois <hansdub@gmail.com>
2015-08-13 08:05:33 +0200

    Cache the directory data for 60 seconds

--- phpstorm.py
+++ phpstorm.py
@@ -3,9 +3,10 @@ import glob
 import json
 from workflow import Workflow, ICON_WEB, web
 
-def main(wf):
+def getProjectDirectories():
     with open('config.json') as data_file:
         data = json.load(data_file)
+        projects = {}
 
         for dir in data['ProjectDirectories']:
             ProjectDirectories = glob.glob(dir + '/*/.idea')
@@ -15,11 +16,21 @@ def main(wf):
                 folderList = projectDirectory.split('/')
                 projectName = folderList[-1]
 
-                wf.add_item(title=projectName,
-                            subtitle=projectDirectory,
-                            icon=ICON_WEB,
-                            arg=projectDirectory,
-                            valid=True)
+                listProject = {projectName, projectDirectory};
+
+                projects[projectDirectory] = projectName
+
+        return projects
+
+def main(wf):
+    projects = wf.cached_data('projects', getProjectDirectories, max_age= -1)
+
+    for dir, name in projects.iteritems():
+        wf.add_item(title=name,
+                    subtitle=dir,
+                    icon=ICON_WEB,
+                    arg=dir,
+                    valid=True)
 
     wf.send_feedback()
 
